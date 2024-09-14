1971d32fbfb765fce8a32608376e1663b242d7fd
funtik0310 <funtik03101996@gmail.com>
2024-09-14 14:19:50 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,32 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor: int):
+        self.new_floor = new_floor
+        if self.new_floor < 1 or self.new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for self.new_floor in range(1, new_floor + 1):
+                print(self.new_floor)
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
